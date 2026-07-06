89989be8d888d288d724bfb9cd5c7db8baa05295
mpower-codeworks <71185882+mpower-codeworks@users.noreply.github.com>
2026-07-06 19:25:43 -0400

    DTE_118

---
+++ DTE/1_0/1_0_BACKUPS/build_118.bat
@@ -0,0 +1,14 @@
+ml /nologo /c /coff /Cp /IC:\masm32\include dte_1_0.asm
+
+crinkler dte_1_0.obj ^
+  /OUT:dte.exe ^
+  /ENTRY:MainEntry ^
+  /SUBSYSTEM:WINDOWS ^
+  /NOINITIALIZERS ^
+  /TINYIMPORT ^
+  /HASHSIZE:8 ^
+  /ORDERTRIES:2000 ^
+  /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.20348.0\um\x86" ^
+  kernel32.lib user32.lib shell32.lib
+
+del dte_1_0.obj
